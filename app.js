1f690a4736322cb5c3cc6238cc1c17cd86416585
ocb-dev-04 <oscarchb04@gmail.com>
2019-10-07 02:46:04 -0400

    *Add all codes and template HTML example

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
+// const feathers = require('@feathersjs/feathers');
+// const express = require('@feathersjs/express');
+// const socketio = require('@feathersjs/socketio');
+// const moment = require('moment');
+
+// // Idea Service
+// class IdeaService {
+//   constructor() {
+//     this.ideas = [];
+//   }
+
+//   async find() {
+//     return this.ideas;
+//   }
+
+//   async create(data) {
+//     const idea = {
+//       id: this.ideas.length,
+//       text: data.text,
+//       tech: data.tech,
+//       viewer: data.viewer
+//     };
+
+//     idea.time = moment().format('h:mm:ss a');
+
+//     this.ideas.push(idea);
+
+//     return idea;
+//   }
+// }
+
+// const app = express(feathers());
+
+// // Parse JSON
+// app.use(express.json());
+// // Config Socket.io realtime APIs
+// app.configure(socketio());
+// // Enable REST services
+// app.configure(express.rest());
+// // Register services
+// app.use('/ideas', new IdeaService());
+
+// // New connections connect to stream channel
+// app.on('connection', conn => app.channel('stream').join(conn));
+// // Publish events to stream
+// app.publish(data => app.channel('stream'));
+
+// const PORT = process.env.PORT || 3030;
+
+// app
+//   .listen(PORT)
+//   .on('listening', () =>
+//     console.log(`Realtime server running on port ${PORT}`)
+//   );
+
+// app.service('ideas').create({
+//   text: 'Build a cool app',
+//   tech: 'Node.js',
+//   viewer: 'John Doe'
+// });
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
@@ -23,26 +83,51 @@ class ServicesNameService {
 
         new_services_name.time = moment().format('h:mm:ss a');
 
-        this.new_services_name.push(idea);
+        this.services_name.push(new_services_name);
 
         return new_services_name;
     }
 }
 
+// ASING EXPRESS WITH FEATHERS
 const app = express(feathers());
+
 // PARSE JSON
 app.use(express.json());
+
 // CONFIG SOCKET.IO REALTIME APIs
 app.configure(socketio());
+
 // ENABLE REST SERVICES
 app.configure(express.rest());
+
 // REGISTER SERVICES
-app.use('/services_name', new ServicesNameService());
+app.use(
+    '/services', 
+    new ServicesNameService()
+);
+
 // NEW CONNECTIONS CONNECT TO STREAM CHANNEL
-app.on('connection', con => app.channel('stream').join(con));
+app.on(
+    'connection', 
+    con => app.channel('stream').join(con)
+);
+
 // PUBLISH EVENTS TO STREAM
 app.publish(data => app.channel('stream'));
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT).on('listenning', () => console.log(`Realtime server running on port ${PORT}`));
+const upServerMsg = () => console.log(`Realtime server running on port ${PORT}`)
+app.listen(PORT).on(
+    'listening', 
+    upServerMsg
+);
+
+// EXAMPLE IDEA DEFAULT
+// app.service('services').create({
+//     text: 'Build a cool app',
+//     tech: 'NodeJS',
+//     viewer: 'John Doe',
+//     time: moment().format('h:mm:ss a')
+// });
